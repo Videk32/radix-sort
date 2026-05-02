18403cf7e9e447a379c08a6d0507ba4b1d42d78a
Videk32 <zaversek.vid@gmail.com>
2026-05-02 11:32:31 +0200

    Dodana github sckripta

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort
+# Build directory: C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(RadixSortTest "C:/Users/zaver/AppData/Local/Programs/Python/Python313/Lib/site-packages/cmake/data/bin/cmake.exe" "-D" "EXECUTABLE=C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort/build/Vaja2_RadixSort.exe" "-D" "TEST_DIR=C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort/tests" "-P" "C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort/tests/run_tests.cmake")
+set_tests_properties(RadixSortTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort/CMakeLists.txt;12;add_test;C:/Users/zaver/OneDrive/Desktop/faks/2. Letnik/2. Polletje/algoritmi/vaja2/radix-sort/CMakeLists.txt;0;")
---
+++ tests/run_tests.cmake
@@ -0,0 +1,72 @@
+file(WRITE "${TEST_DIR}/input1.txt" "5 3 8 1 2 255 0 10")
+
+execute_process(
+        COMMAND "${EXECUTABLE}" "${TEST_DIR}/input1.txt"
+        WORKING_DIRECTORY "${TEST_DIR}"
+        RESULT_VARIABLE result
+)
+
+if(NOT result EQUAL 0)
+    message(FATAL_ERROR "Program se ni uspešno izvedel pri testu 1.")
+endif()
+
+file(READ "${TEST_DIR}/out.txt" output)
+string(STRIP "${output}" output)
+
+if(NOT output STREQUAL "0 1 2 3 5 8 10 255")
+    message(FATAL_ERROR "Test 1 ni uspel. Dobljeno: ${output}")
+endif()
+
+
+file(WRITE "${TEST_DIR}/input2.txt" "1 2 3 4 5")
+
+execute_process(
+        COMMAND "${EXECUTABLE}" "${TEST_DIR}/input2.txt"
+        WORKING_DIRECTORY "${TEST_DIR}"
+        RESULT_VARIABLE result
+)
+
+if(NOT result EQUAL 0)
+    message(FATAL_ERROR "Program se ni uspešno izvedel pri testu 2.")
+endif()
+
+file(READ "${TEST_DIR}/out.txt" output)
+string(STRIP "${output}" output)
+
+if(NOT output STREQUAL "1 2 3 4 5")
+    message(FATAL_ERROR "Test 2 ni uspel. Dobljeno: ${output}")
+endif()
+
+
+file(WRITE "${TEST_DIR}/input3.txt" "9 1 9 3 1 0")
+
+execute_process(
+        COMMAND "${EXECUTABLE}" "${TEST_DIR}/input3.txt"
+        WORKING_DIRECTORY "${TEST_DIR}"
+        RESULT_VARIABLE result
+)
+
+if(NOT result EQUAL 0)
+    message(FATAL_ERROR "Program se ni uspešno izvedel pri testu 3.")
+endif()
+
+file(READ "${TEST_DIR}/out.txt" output)
+string(STRIP "${output}" output)
+
+if(NOT output STREQUAL "0 1 1 3 9 9")
+    message(FATAL_ERROR "Test 3 ni uspel. Dobljeno: ${output}")
+endif()
+
+file(WRITE "${TEST_DIR}/input4.txt" "1 2 300")
+
+execute_process(
+        COMMAND "${EXECUTABLE}" "${TEST_DIR}/input4.txt"
+        WORKING_DIRECTORY "${TEST_DIR}"
+        RESULT_VARIABLE result
+)
+
+if(result EQUAL 0)
+    message(FATAL_ERROR "Test 4 ni uspel. Program bi moral vrniti napako za število 300.")
+endif()
+
+message("Vsi testi so uspešno prestali preverjanje.")
